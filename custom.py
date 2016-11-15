e90d2c67a35926feaa24cb25264999b79f4931c9
kim <kim@marza.com>
2016-11-15 18:56:57 +0900

    added custom.py for OPTIONS

---
+++ custom.py
@@ -0,0 +1,68 @@
+import os
+import platform
+
+
+# set these variables
+arnold_dir = ""
+depn_dir = ""
+
+
+if platform.system() == "Linux":
+    platform = "linux"
+else:
+    platform = "osx"
+
+# depn dirs
+dep_lib = depn_dir + "/lib"
+dep_include = depn_dir + "/include"
+
+# include path
+TBB_INCLUDE_PATH = dep_include
+BOOST_INCLUDE_PATH = dep_include
+OPENEXR_INCLUDE_PATH = dep_include
+ILMBASE_INCLUDE_PATH = dep_include
+JPEG_INCLUDE_PATH = dep_include
+PNG_INCLUDE_PATH = dep_include
+TIFF_INCLUDE_PATH = dep_include
+FREETYPE_INCLUDE_PATH = dep_include + "/freetype2"
+OSL_INCLUDE_PATH = dep_include
+OIIO_INCLUDE_PATH = dep_include
+GLEW_INCLUDE_PATH = dep_include + "/GL"
+ALEMBIC_INCLUDE_PATH = dep_include
+HDF5_INCLUDE_PATH = dep_include
+APPLESEED_INCLUDE_PATH = depn_dir + "/appleseed/include"
+
+# lib path
+TBB_LIB_PATH = dep_lib
+BOOST_LIB_PATH = dep_lib
+OPENEXR_LIB_PATH = dep_lib
+ILMBASE_LIB_PATH = dep_lib
+JPEG_LIB_PATH = dep_lib
+PNG_LIB_PATH = dep_lib
+TIFF_LIB_PATH = dep_lib
+FREETYPE_LIB_PATH = dep_lib
+OSL_LIB_PATH = dep_lib
+OIIO_LIB_PATH = dep_lib
+GLEW_LIB_PATH = dep_lib
+ALEMBIC_LIB_PATH = dep_lib
+HDF5_LIB_PATH = dep_lib
+APPLESEED_LIB_PATH = depn_dir + "/appleseed/lib"
+
+# suffix
+TBB_LIB_SUFFIX = ""
+BOOST_LIB_SUFFIX = ""
+OPENEXR_LIB_SUFFIX = ""
+PNG_LIB_SUFFIX = ""
+GLEW_LIB_SUFFIX = ""
+ALEMBIC_LIB_SUFFIX = ""
+HDF5_LIB_SUFFIX = ""
+
+# arnold
+ARNOLD_ROOT = arnold_dir
+
+# with gl
+WITH_GL = True
+
+# build dir
+INSTALL_PREFIX = os.path.abspath("dist")
+BUILD_CACHEDIR = os.path.abspath("build")
